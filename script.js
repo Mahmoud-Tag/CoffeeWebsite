eb2e916789a2654221a91c99391797e1c06af0c5
Mahmoud Tag <55888427+Mahmoud-Tag@users.noreply.github.com>
2023-05-13 22:06:20 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// function to run loading-div 
 gsap.fromTo(
   "#loading",
   { opacity: 1 },
@@ -7,3 +8,11 @@ gsap.fromTo(
     delay: 4,
   }
 );
+
+
+// hidden loading-div after 6 sec 
+const myTimeout = setTimeout(loading, 6000);
+
+function loading() {
+  document.getElementById("loading").style.display = "none";
+}
